cb21e94f9940e33777f4c09cde89e252901b346d
Davide Bettio <davide@uninstall.it>
2020-02-28 23:58:57 +0100

    Use new Elixir PackBEAM in mix atomvm.packbeam task
    Do not rely on external tool.

--- lib/mix/tasks/packbeam.ex
+++ lib/mix/tasks/packbeam.ex
@@ -9,25 +9,11 @@ defmodule Mix.Tasks.Atomvm.Packbeam do
          config = Project.config(),
          {:atomvm, {:ok, avm_config}} <- {:atomvm, Keyword.fetch(config, :atomvm)},
          {:start, {:ok, start_module}} <- {:start, Keyword.fetch(avm_config, :start)},
-         install_prefix when install_prefix != nil <- System.get_env("ATOMVM_INSTALL_PREFIX") do
-      avms_path = Path.join(install_prefix, "lib/AtomVM/ebin/")
-
-      start_beam_file = "#{Atom.to_string(start_module)}.beam"
-
-      avms_path
-      |> File.ls!()
-      |> Enum.map(fn file -> Path.join(avms_path, file) end)
-      |> PackBEAM.make_avm("deps.avm")
-
-      Project.compile_path()
-      |> File.ls!()
-      |> Enum.filter(fn file -> String.ends_with?(file, ".beam") end)
-      |> List.delete(start_beam_file)
-      |> List.insert_at(0, start_beam_file)
-      |> Enum.map(fn file -> Path.join(Project.compile_path(), file) end)
-      |> Enum.concat(["deps.avm"])
-      |> PackBEAM.make_avm("#{config[:app]}.avm")
-
+         install_prefix when install_prefix != nil <- System.get_env("ATOMVM_INSTALL_PREFIX"),
+         avms_path = Path.join(install_prefix, "lib/AtomVM/ebin/"),
+         :ok <- pack_deps(avms_path),
+         start_beam_file = "#{Atom.to_string(start_module)}.beam",
+         :ok <- pack_beams(Project.compile_path(), start_beam_file, "#{config[:app]}.avm") do
       {:ok, []}
     else
       {:check, _} ->
@@ -45,6 +31,28 @@ defmodule Mix.Tasks.Atomvm.Packbeam do
       nil ->
         IO.puts("error: ATOMVM_INSTALL_PREFIX env var is not set.")
         :error
+
+      error ->
+        IO.puts("error: unexpected error: #{inspect(error)}.")
+        :error
     end
+  end
+
+  defp pack_deps(avms_path) do
+    avms_path
+    |> File.ls!()
+    |> Enum.map(fn file -> Path.join(avms_path, file) end)
+    |> PackBEAM.make_avm("deps.avm")
+  end
+
+  defp pack_beams(beams_path, start_beam_file, out) do
+    beams_path
+    |> File.ls!()
+    |> Enum.filter(fn file -> String.ends_with?(file, ".beam") end)
+    |> List.delete(start_beam_file)
+    |> List.insert_at(0, start_beam_file)
+    |> Enum.map(fn file -> Path.join(Project.compile_path(), file) end)
+    |> Enum.concat(["deps.avm"])
+    |> PackBEAM.make_avm(out)
   end
 end
