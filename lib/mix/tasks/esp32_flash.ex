f6da2b395d5b94f84b09dc0ade9fc5d21cf3570e
Davide Bettio <davide@uninstall.it>
2022-05-23 00:35:37 +0200

    Added command line options for mix tasks and documentation.
    Also removed requirement for having IDF_PATH set.  If not set, then esptool.py will be used from the user's PATH.
    
    Signed-off-by: Fred Dushin <fred@dushin.net>

--- lib/mix/tasks/esp32_flash.ex
+++ lib/mix/tasks/esp32_flash.ex
@@ -9,33 +9,37 @@ defmodule Mix.Tasks.Atomvm.Esp32.Flash do
     config = Project.config()
 
     with {:atomvm, {:ok, avm_config}} <- {:atomvm, Keyword.fetch(config, :atomvm)},
+         {:args, {:ok, options}} <- {:args, parse_args(args)},
          {:pack, {:ok, _}} <- {:pack, Packbeam.run(args)},
-         idf_path when idf_path != nil <- System.get_env("IDF_PATH") do
-      flash_offset = Keyword.get(avm_config, :flash_offset, 0x110000)
-      flash(idf_path, flash_offset)
+         idf_path <- System.get_env("IDF_PATH", <<"">>) do
+      chip = Map.get(options, :chip, Keyword.get(avm_config, :chip, "esp32"))
+      port = Map.get(options, :port, Keyword.get(avm_config, :port, "/dev/ttyUSB0"))
+      baud = Map.get(options, :baud, Keyword.get(avm_config, :baud, "115200"))
+      flash_offset = Map.get(options, :flash_offset, Keyword.get(avm_config, :flash_offset, 0x210000))
+      flash(idf_path, chip, port, baud, flash_offset)
     else
       {:atomvm, :error} ->
         IO.puts("error: missing AtomVM project config.")
         :error
 
-      {:pack, _} ->
-        IO.puts("error: failed PackBEAM, target will not be flashed.")
+      {:args, :error} ->
+        IO.puts("Syntax: ")
         :error
 
-      nil ->
-        IO.puts("error: IDF_PATH env var is not set.")
+      {:pack, _} ->
+        IO.puts("error: failed PackBEAM, target will not be flashed.")
         :error
     end
   end
 
-  def flash(idf_path, flash_offset) do
+  def flash(idf_path, chip, port, baud, flash_offset) do
     tool_args = [
       "--chip",
-      "esp32",
+      chip,
       "--port",
-      "/dev/ttyUSB0",
+      port,
       "--baud",
-      "115200",
+      baud,
       "--before",
       "default_reset",
       "--after",
@@ -51,8 +55,43 @@ defmodule Mix.Tasks.Atomvm.Esp32.Flash do
       "0x#{Integer.to_string(flash_offset, 16)}",
       "#{Project.config()[:app]}.avm"
     ]
-
-    tool_full_path = "#{idf_path}#{@esp_tool_path}"
+    tool_full_path = get_esptool_path(idf_path)
     System.cmd(tool_full_path, tool_args, stderr_to_stdout: true, into: IO.stream(:stdio, 1))
+  end
+
+  defp get_esptool_path(<<"">>) do
+    "esptool.py"
+  end
+
+  defp get_esptool_path(idf_path) do
+    "#{idf_path}#{@esp_tool_path}"
+  end
+
+  defp parse_args(args) do
+    parse_args(args, %{})
+  end
+
+  defp parse_args([], accum) do
+    {:ok, accum}
+  end
+
+  defp parse_args([<<"--port">>, port | t], accum) do
+    parse_args(t, Map.put(accum, :port, port))
+  end
+
+  defp parse_args([<<"--baud">>, baud | t], accum) do
+    parse_args(t, Map.put(accum, :baud, baud))
+  end
+
+  defp parse_args([<<"--chip">>, chip | t], accum) do
+    parse_args(t, Map.put(accum, :chip, chip))
+  end
+
+  defp parse_args([<<"--flash_offset">>, flash_offset | t], accum) do
+    parse_args(t, Map.put(accum, :flash_offset, flash_offset))
+  end
+
+  defp parse_args([_|t], accum) do
+    parse_args(t, accum)
   end
 end
