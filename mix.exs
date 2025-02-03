482c4396da6135aa9cb09656f1c3a5e53e4565cc
Joost Oostdijk <joustava@gmail.com>
2025-02-03 15:42:32 +0200

    Use the Config module instead of Mix.Config is deprecated
    Gets rid of:
    warning: use Mix.Config is deprecated. Use the Config module instead
      config/config.exs:3

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ExAtomVM.MixProject do
     [
       app: :exatomvm,
       version: "0.1.0",
-      elixir: "~> 1.8",
+      elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -21,7 +21,7 @@ defmodule ExAtomVM.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:uf2tool,  "1.1.0"}
+      {:uf2tool, "1.1.0"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
